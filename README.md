6052bbb3ba1ffa5be941f694c4724732a225e5ce
Taran Pierce <taran.pierce@endurance.com>
2017-07-15 22:38:30 -0600

    master
    First commit setting up most of what is needed to get a site up and running quickly

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # jump-start
+
+# Install dependencies
+npm install
+
+# Directory structure should look as follows:
+
+  /project
+  |
+  |-app
+  |  |-scss
+  |  |-js
+  |  |-css
+  |  |-images
+  |  |-fonts
+  |
+  |-dist
+  |  |-css
+  |  |-js
+  |  |-fonts
+  |  |-images
+
+# Gulp Commands
+gulp
+
+This will build the project, turn on browserSync and set up watcher so any edits you make will trigger the proper recomipling and window reloading
+
+# Sass
+Using sass for css preprocessing
+
+# Uglify
+Using uglify for minifyig the JS
+
+# Imagemin
+Using Imagemin for optimizing the images
+
+# Cache
+Using cache so we only optimize images that are new
+
+# cssnano to minify css
+Using cssnano to minify the css
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var browserSync  = require('browser-sync').create();
+var useref       = require('gulp-useref');
+var gulpIf       = require('gulp-if');
+var uglify       = require('gulp-uglify');
+var cssnano      = require('gulp-cssnano');
+var imagemin     = require('gulp-imagemin');
+var cache        = require('gulp-cache');
+var del          = require('del');
+var runSequence = require('run-sequence');
+
+// generic example
+//gulp.task('task-name', function () {
+//  return gulp.src('source-files') // Get source files with gulp.src
+//    .pipe(aGulpPlugin()) // Sends it through a gulp plugin
+//    .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
+//})
+
+// compile sass to css
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// watch app and run different tasks
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/**/*.html', ['useref']);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// live reload
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  })
+});
+
+// concat files and ugilfy them
+gulp.task('useref', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// optimze images
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpg|gif|svg)')
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// move fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+
+// clean dist directory
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+// build project
+gulp.task('build', function(callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
+
+// default task for easy start
+gulp.task('default', function(callback) {
+  runSequence(
+    ['build', 'sass', 'browserSync', 'watch'],
+    callback
+  )
+});
