973c9f5400fcb45f5264217d4d7c4f3a07fc9061
Taran Pierce <taran.pierce@endurance.com>
2017-08-02 00:48:14 -0600

    feature/convert-to-php
    Working on a few css updates
    
    Somethign is wrong with the .htaccess update, it doesn't seem to be properly updating the page any more

--- app/index.php
+++ app/index.php
@@ -9,4 +9,11 @@
     <h2>What makes it easy to start a website?</h2>
     <p>There are quite a few things to take advantage of to help you get your website up and running. Let's take a look at some of my favorite things that I think are the most useful.</p>
   </div>
+  <div class="container-fluid secondary">
+    <div class="container">
+      <h2>Just seems like something</h2>
+      <p>Doesn't it though?</p>
+      <p>This just feels like there is going to be some kind of element or style that I end up using here that will make sense.</p>
+    </div>
+  </div>
 <?php include 'templates/bottom.php' ?>
